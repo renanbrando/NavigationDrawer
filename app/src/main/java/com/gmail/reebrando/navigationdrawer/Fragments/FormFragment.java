36a7a4ea1737f4151550fb3e1166ae6b7605da72
Renan Brando <reebrando@gmail.com>
2017-06-05 22:59:42 -0300

    Added calculator activity.

--- app/src/main/java/com/gmail/reebrando/navigationdrawer/Fragments/FormFragment.java
+++ app/src/main/java/com/gmail/reebrando/navigationdrawer/Fragments/FormFragment.java
@@ -1,18 +1,25 @@
 package com.gmail.reebrando.navigationdrawer.Fragments;
 
-
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gmail.reebrando.navigationdrawer.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FormFragment extends Fragment {
+public class FormFragment extends Fragment implements View.OnClickListener {
+    private EditText ed1;
+    private EditText ed2;
+    private TextView txtResult;
+    private Button btnPlus;
+    private Button btnMinus;
+    private Button btnMultiply;
+    private Button btnDivide;
 
 
     public FormFragment() {
@@ -24,7 +31,52 @@ public class FormFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form, container, false);
+        View view = inflater.inflate(R.layout.fragment_form, container, false);
+        ed1 = (EditText) view.findViewById(R.id.edOne);
+        ed2 = (EditText) view.findViewById(R.id.edTwo);
+        txtResult = (TextView) view.findViewById(R.id.txtResult);
+        btnPlus = (Button) view.findViewById(R.id.btnPlus);
+        btnMinus = (Button) view.findViewById(R.id.btnMinus);
+        btnMultiply = (Button) view.findViewById(R.id.btnMultiply);
+        btnDivide = (Button) view.findViewById(R.id.btnDivide);
+
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
+
+        return view;
     }
 
+
+    @Override
+    public void onClick(View v) {
+        double num1 = 0;
+        double num2 = 0;
+        if (!ed1.getText().toString().isEmpty() && !ed2.getText().toString().isEmpty()) {
+            num1 = Double.parseDouble(ed1.getText().toString());
+            num2 = Double.parseDouble(ed2.getText().toString());
+        }
+        else{
+            Snackbar.make(v, "Do not screw with me, alright?", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+        switch (v.getId()) {
+            case R.id.btnPlus:
+                txtResult.setText(String.valueOf(num1+num2));
+                break;
+            case R.id.btnMinus:
+                txtResult.setText(String.valueOf(num1-num2));
+                break;
+            case R.id.btnMultiply:
+                txtResult.setText(String.valueOf(num1*num2));
+                break;
+            case R.id.btnDivide:
+                txtResult.setText(String.valueOf(num1/num2));
+                break;
+            default:
+                break;
+        }
+
+    }
 }
--- app/src/main/java/com/gmail/reebrando/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/gmail/reebrando/navigationdrawer/MainActivity.java
@@ -1,8 +1,10 @@
 package com.gmail.reebrando.navigationdrawer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,10 +14,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.gmail.reebrando.navigationdrawer.Fragments.FormFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private RelativeLayout content_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        content_main = (RelativeLayout) findViewById(R.id.content_main);
     }
 
     @Override
@@ -80,18 +88,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id){
+            case R.id.nav_calculadora:
+                FormFragment formFragment = new FormFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_main, formFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                break;
+            default:
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
